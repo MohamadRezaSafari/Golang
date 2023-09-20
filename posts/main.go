29c32e6d5a6acf5f1f367d324334e0729fb9155c
Reza <Reza@DESKTOP-CKL538G>
2023-09-20 17:22:23 +0330

    First Golang api

--- posts/main.go
+++ posts/main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"log"
 
-	"github.com/gofiber/fiber"
-	"gorm.io/driver/sqlserver"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 type Post struct {
-	Id          uint
-	Title       string
-	Description string
+	Id          uint   `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
 }
 
 func main() {
 
-	db, err := gorm.Open(sqlserver.Open("sa:V_f=^t%Fam!2GZ%@tcp(127.0.0.1:1433)/posts_ms"), &gorm.Config{})
+	dsn := "host=localhost user=postgres password=1 dbname=posts_ms port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
@@ -25,10 +27,30 @@ func main() {
 	db.AutoMigrate(Post{})
 
 	app := fiber.New()
+	app.Use(cors.New())
 
-	// app.Get("/", func(c *fiber.Ctx) error {
-	// 	return c.SendString("Hi")
-	// })
+	app.Get("/api/posts", func(c *fiber.Ctx) error {
+
+		var posts []Post
+
+		db.Find(&posts)
+
+		return c.JSON(posts)
+	})
+
+	app.Post("/api/posts/create", func(c *fiber.Ctx) error {
+
+		var post Post
+
+		if err := c.BodyParser(&post); err != nil {
+			return err
+		}
+
+		db.Create(&post)
+
+		return c.JSON(post)
+
+	})
 
 	log.Fatal(app.Listen(":5241"))
 }
